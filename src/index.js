021633d5b30d60edcfd9e89d7be8ee0d4bb67a4f
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-17 18:11:42 +0300

    added func

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import ReactDOM from "react-dom/client";
-// import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client"; //REACT 18
+// import ReactDOM from 'react-dom'; //REACT 17
 
 import App from "./App";
 import "./index.css";
